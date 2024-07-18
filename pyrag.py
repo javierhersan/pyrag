809f953798132c9dc9f93402a54ebebe97bdea51
Javier Hernández Sánchez <javier.hernandezsanchez@outlook.com>
2024-07-18 18:37:09 +0200

    feat(pyrag): add pyrag function execution v0.2.0

--- pyrag.py
+++ pyrag.py
@@ -4,6 +4,7 @@ from openai import OpenAI
 import faiss
 import numpy as np
 import requests
+import re
 
 def main():
     # Load the .env file
@@ -24,12 +25,19 @@ def main():
     print(f"------------------------------------------------------------------------------")
     print(f"Storing relevant documents...")
     documents = [
-        "If the user wants to execute this function answer with: <execute> transaction(address, amount) </execute>", 
+        """
+            If the user wants to make a transaction (amount always in USD) answer with: <execute> transaction(address:str, usd_amount:int) </execute>
+        """, 
         """This is a list of Zurich Business Units with its relevant bank accounts:
             Zurich Barcelona: ES00 0000 0000 0000
             Zurich Honk Kong: HK28 0000 0000 0000
             Zurich North America: NA99 0000 0000 0000
+        """,
         """
+            FX Rates:
+            USD to EUR: 1.10
+            AUD to EUR: 1.50
+        """, 
     ]
     print(f"Relevant documents: \n")
     for doc in documents:
@@ -59,8 +67,9 @@ def main():
         relevant_documents = query_embeddings(query, client, index, documents)
         print(f"> Bot: Relevant documents found: {relevant_documents}")
 
-        response = generate_response(query, client, relevant_documents)
-        print(f"> Bot: {response}")
+        response = generate_response(conversation_history, client, relevant_documents)
+        was_tool_executed = execute_tools(response)
+        if (not was_tool_executed): print(f"> Bot: {response}")
         conversation_history.append(f"> Bot: {response}")
 
 def get_embeddings(documents:list[str], client, model="text-embedding-ada-002"):
@@ -71,7 +80,7 @@ def get_embeddings(documents:list[str], client, model="text-embedding-ada-002"):
     embeddings = np.array([data.embedding for data in response.data])
     return embeddings
 
-def query_embeddings(query, client, index, documents, k=2):
+def query_embeddings(query, client, index, documents, k=3):
     query_embedding = get_embeddings([query], client)[0].reshape(1, -1)
     distances, indices = index.search(query_embedding, k)
     return [documents[i] for i in indices[0]]
@@ -94,6 +103,31 @@ def generate_response(conversation_history, client, relevant_documents):
     )
     return response.choices[0].message.content
 
+def execute_tools(text:str):
+    pattern = r'<execute>\s*(.*?)\s*</execute>'
+    match = re.search(pattern, text)
+    if match:
+        code_to_execute = match.group(1).strip()
+        code_to_execute = re.sub(r'\s+', ' ', code_to_execute)
+
+        print(f"> Bot: Selecting tool: {code_to_execute}")
+        print(f"> Bot: Executing tool...")
+        # print(f"> Tool: 0%")
+        # print(f"> Tool: 0% ↻ 25%")
+        # print(f"> Tool: 0% ↻ 25% ↻ 50%")
+        # print(f"> Tool: 0% ↻ 25% ↻ 50% ↻ 75%")
+        # print(f"> Tool: 0% ↻ 25% ↻ 50% ↻ 75% ↻ 100% ")
+        
+        # Define the transaction function
+        def transaction(address, usd_amount):
+            print(f"> Tool: Transaction executed | address={address}, amount={usd_amount}")
+
+        # Execute the extracted code
+        exec(code_to_execute)
+        print("> Bot: Tool executed")
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
